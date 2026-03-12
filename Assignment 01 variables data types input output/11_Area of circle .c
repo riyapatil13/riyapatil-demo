e9ee6333ca4a22c2dbfcce1fdf7d128c360d4fc5
Riya Patil <riyapatil34812@gmail.com>
2026-03-12 19:33:28 +0530

    Add files via upload

---
+++ Assignment 01 variables data types input output/11_Area of circle .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    float Rad,Area,PI = 3.14;
+
+    printf("\n Enter Radius Of Circle");
+    scanf("%f",&Rad);
+
+    Area = PI*Rad*Rad;
+
+    printf("\n Area Circle with radius = %0.2f", Area);
+  
+    getch ()
+    return 0;
+}
---
+++ Assignment 01 variables data types input output/12_circumference of circle .c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    float Rad,Circum,PI = 3.14;
+
+    printf("\n Enter Radius Of Circle");
+    scanf("%f",&Rad);
+
+    Circum = 2*PI*Rad ;
+
+    printf("\n circumference Circle with radius = %0.2f",  Circum);
+  
+    getch ()
+    return 0;
+}
