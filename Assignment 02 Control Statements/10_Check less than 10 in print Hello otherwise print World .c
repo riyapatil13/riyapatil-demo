0deb655ec43d270a06afb92879549c6a08102c50
Riya Patil <riyapatil34812@gmail.com>
2026-03-13 22:48:16 +0530

    Add files via upload

---
+++ Assignment 02 Control Statements/10_Check less than 10 in print Hello otherwise print World .c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int Number;
+
+    printf("\n Enter the  Number");
+    scanf("%d",&Number);
+
+    if(Num <=10)
+    {
+        printf("\n\tHello");
+    }
+    else
+    {
+        printf("\n\t World");
+    }
+
+    getch();
+    return 0;
+}
