6ee1bdd9987e65ec661006fa307727860d7605a4
Riya Patil <riyapatil34812@gmail.com>
2026-03-17 17:31:22 +0530

    Add files via upload

---
+++ Assignment 01 variables data types input output/07_ASCII Table  (1).c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int i=0;
+
+    printf("\n ASCII Table");
+
+    while(i <=127)
+    {
+        printf("\n\t  %d :'%c' ",i, i);
+        i++;
+    }    
+
+    getch();
+    return 0;
+}
---
+++ Assignment 01 variables data types input output/09_Character for ASCII Value .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+  int num;
+
+printf(" \n Enter the number");
+scanf("%d",&num);
+
+printf("\nASCII Value of %d is= %c",num,num);
+
+getch();
+return 0;
+}
