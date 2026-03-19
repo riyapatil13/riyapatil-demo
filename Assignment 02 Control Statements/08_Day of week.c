3f4184e667e1bef367530fd04c07af8c14292817
Riya Patil <riyapatil34812@gmail.com>
2026-03-19 20:03:09 +0530

    Add files via upload

---
+++ Assignment 02 Control Statements/08_Day of week.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num;
+    printf("\n\t Enter the Number");
+    scanf("%d",&Num);
+
+    if(Num==1)
+    {
+        printf("\n Day of Monday");
+    }
+
+    else if(Num==2)
+
+    {
+        printf("\n Day of Tuesday");
+    }
+
+    else if(Num==3)
+
+    {
+      printf("\n Day of wensaday");
+    }
+
+    else if(Num==4)
+
+    {
+      printf("\n Day of Thursday");
+    }
+
+    else if(Num==5)
+
+    {
+      printf("\n Day of Friday");
+    }
+
+    else if(Num==6)
+
+    {
+      printf("\n Day of Saturday");
+
+    }
+    else if(Num==7)
+    {
+      printf("\n Day of Sunday");
+
+    }
+
+    else
+    {
+        printf("\nGiven the Number is default");
+    }
+
+   getch ()
+    return 0;
+
+    }
---
+++ Assignment 02 Control Statements/09_Month number corresponding  Month Name .c
@@ -0,0 +1,85 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int Num;
+    printf("\n\t Enter the Number");
+    scanf("%d",&Num);
+
+    if(Num==1)
+    {
+        printf("\n Month of January");
+    }
+
+    else if(Num==2)
+
+    {
+        printf("\n Month of February");
+    }
+
+    else if(Num==3)
+
+    {
+      printf("\n Month of March");
+    }
+
+    else if(Num==4)
+
+    {
+      printf("\n Month of April");
+    }
+
+    else if(Num==5)
+
+    {
+      printf("\n Month of May");
+    }
+
+    else if(Num==6)
+
+    {
+      printf("\n Month of June");
+
+    }
+    else if(Num==7)
+    {
+      printf("\n Month of July");
+
+    }
+    else if(Num==8)
+    {
+      printf("\n Month of August");
+
+    }
+    else if(Num==9)
+    {
+      printf("\n Month of September");
+
+    }
+    else if(Num==10)
+    {
+      printf("\n Month of October");
+
+    }
+    else if(Num==11)
+    {
+      printf("\n Month of November");
+
+    }
+    else if(Num==12)
+    {
+      printf("\n Month of December");
+
+    }
+    else 
+    
+    {
+    
+        printf("\nGiven the Number is default");
+ 
+    }
+    
+    getch();
+    return 0;
+
+    }
