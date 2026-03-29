6651b87223c06ce07de363e6e1afcd7b3ff67258
Riya Patil <riyapatil34812@gmail.com>
2026-03-29 10:30:12 +0530

    Add files via upload

---
+++ Assignment 03 iteration of loop/01_Print the number from 101 to 110 (1).c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdio.h>
+int main ()
+{
+    int No;
+    printf("\n\tEnter the Number");
+    
+    No=101;
+    while (No<=110)
+    {
+        printf("\n %d",No);
+        No++;
+    }
+    getch();
+    return 0;
+    
+}
