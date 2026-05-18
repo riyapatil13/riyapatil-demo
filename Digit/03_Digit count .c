0707e530149f2180594859fc44cb072bb2459a6b
Riya Patil <riyapatil34812@gmail.com>
2026-05-18 15:48:46 +0530

    Add files via upload

---
+++ Digit/03_Digit count .c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+
+int main()
+{
+    int no, count = 0;
+
+    printf("Enter number ");
+    scanf("%d", &no);
+
+    while(no > 0)
+    {
+        count++;
+        no = no / 10;
+    }
+
+    printf("Count = %d", count);
+    
+    getch ();
+    return 0;
+}
---
+++ Digit/04_Even digit count .c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    int no, digit, count = 0;
+
+    printf("Enter number ");
+    scanf("%d", &no);
+
+    while(no > 0)
+    {
+        digit = no % 10;
+
+        if(digit % 2 == 0)
+        {
+            count++;
+        }
+
+        no = no / 10;
+    }
+
+    printf("Even digits = %d", count);
+    
+    getch ();
+    return 0;
+}
