1afd2e47cfd0b3686650dfcc2f7a41e8f2158636
Riya Patil <riyapatil34812@gmail.com>
2026-05-18 16:12:16 +0530

    Add files via upload

---
+++ Assignment 05 Digit/02_Display the  multiplication digit.c
@@ -0,0 +1,22 @@
+#include<conio.h>
+#include<stdio.h>
+
+int main()
+{
+    int no, digit, mult = 1;
+
+    printf("Enter number : ");
+    scanf("%d", &no);
+
+    while(no > 0)
+    {
+        digit = no % 10;
+        mult = mult * digit;
+        no = no / 10;
+    }
+
+    printf("Multiplication = %d", mult);
+    
+    getch ();
+    return 0;
+}
